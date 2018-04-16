e5a4274f28d53bc36b35bc137ebbcbe9d1911083
Koomi <koomiadeh@gmail.com>
2018-04-16 03:46:06 +0200

    debuter

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    posted = db.Column(db.Boolean)
+
+    def __init__(self, name):
+        self.name = name
+        self.posted = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():

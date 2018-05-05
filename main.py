869887d67cf42ae6fc43b7b4d372f9d234bfcb2c
Shawn Jones <shawn.greg.jones@gmail.com>
2018-05-05 08:26:00 -0500

    change port number

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'danken'
